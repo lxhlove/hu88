95f19a08706c42ada0056c4c7d67c0c7c3dd0f35
xiaohu <473000220.com>
2019-08-22 20:24:51 +0800

    初始化目录

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,7 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <!-- 根组件 -->
+  <!-- 定义一级路由的出口(组件渲染的位置) -->
+  <router-view></router-view>
 </template>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="less"></style>
